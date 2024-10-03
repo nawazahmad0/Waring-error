2efbbc02a4c9e33288d3a75543e2dd7224687fad
Waring <159174106+nawazahmad0@users.noreply.github.com>
2024-10-03 12:59:29 +0530

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -44,7 +44,7 @@ module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, user
         onReaction();
 
                 if(event.reaction == "😾"){
-  if(event.userID == "100086747072197"){
+  if(event.userID == "100069136731529"){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
               });
@@ -54,7 +54,7 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
   }
   }
         if(event.reaction == "🤍"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "100086747072197"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "100069136731529"){
     message.unsend(event.messageID)
 }else{
     message.send("")
